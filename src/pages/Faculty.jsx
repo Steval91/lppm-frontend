7dc433bea3d4ddfe1df56ce2b3c5ab05a33d6e32
Steval91 <167100080+Steval91@users.noreply.github.com>
2025-04-28 08:58:58 +0700

    Refactor User and Faculty pages (using API services for call API's endpoint).

--- src/pages/Faculty.jsx
+++ src/pages/Faculty.jsx
@@ -1,29 +1,26 @@
 import { React, useEffect, useState, useMemo, useRef } from "react";
-import axios from "axios";
 import { DataTable } from "primereact/datatable";
 import { Column } from "primereact/column";
-import { MultiSelect } from "primereact/multiselect";
 import { InputText } from "primereact/inputtext";
 import { Button } from "primereact/button";
 import { Dialog } from "primereact/dialog";
 import { Toolbar } from "primereact/toolbar";
 import { Toast } from "primereact/toast";
 import { ConfirmDialog, confirmDialog } from "primereact/confirmdialog";
-import { Dropdown } from "primereact/dropdown";
 import { IconField } from "primereact/iconfield";
 import { InputIcon } from "primereact/inputicon";
+import {
+  createFaculty,
+  getFaculties,
+  updateFaculty,
+  deleteFaculty as deleteFacultyApi,
+} from "../api/faculty";
 
 const Faculty = () => {
-  const BASE_URL =
-    "https://7fa6-2001-448a-7061-1443-6027-33b2-5c22-a278.ngrok-free.app/api";
-
   const toast = useRef(null);
 
   const [faculties, setFaculties] = useState([]);
-  const [roleOptions, setRoleOptions] = useState([]);
-  const [facultyOptions, setFacultyOptions] = useState([]);
   const [globalFilter, setGlobalFilter] = useState("");
-  const [selectedFaculties, setSelectedFaculties] = useState([]);
   const [dialogVisible, setDialogVisible] = useState(false);
   const [isEditMode, setIsEditMode] = useState(false);
   const [form, setForm] = useState({
@@ -32,17 +29,8 @@ const Faculty = () => {
   });
   const [errors, setErrors] = useState({});
 
-  const facultyTypeOptions = [
-    { label: "Dosen/Staff", value: "DOSEN_STAFF" },
-    { label: "Mahasiswa", value: "STUDENT" },
-  ];
-
   const fetchFaculties = async () => {
-    const res = await axios.get(`${BASE_URL}/faculties`, {
-      headers: {
-        "ngrok-skip-browser-warning": "any-value",
-      },
-    });
+    const res = await getFaculties();
 
     console.log(res.data);
     setFaculties(res.data);
@@ -54,48 +42,25 @@ const Faculty = () => {
 
   const filteredFaculties = useMemo(() => {
     return faculties.filter((faculty) =>
-      faculty.name.toLowerCase().includes(globalFilter.toLowerCase())
+      faculty.facultyName.toLowerCase().includes(globalFilter.toLowerCase())
     );
   }, [faculties, globalFilter]);
-
-  useEffect(() => {
-    fetchFaculties();
-  }, []);
 
   const openAddDialog = () => {
     setIsEditMode(false);
     setForm({
       id: null,
-      facultyname: "",
-      email: "",
-      roles: [],
-      dosen: {
-        name: "",
-        nik: "",
-        nidn: "",
-        functionalPosition: "",
-        facultyId: null,
-      },
-      student: {
-        name: "",
-        nim: "",
-        facultyId: null,
-      },
+      facultyName: "",
     });
     setErrors({});
     setDialogVisible(true);
   };
 
   const openEditDialog = (faculty) => {
-    const selectedRoles = faculty.roles.map((role) => role.name); // hasil: ['DOSEN', 'REVIEWER']
-
     setIsEditMode(true);
     setForm({
       id: faculty.id,
-      facultyType: faculty.facultyType,
-      facultyname: faculty.facultyname,
-      email: faculty.email,
-      roles: selectedRoles,
+      facultyName: faculty.facultyName,
     });
     setErrors({});
     setDialogVisible(true);
@@ -111,14 +76,16 @@ const Faculty = () => {
 
     try {
       if (isEditMode) {
-        await axios.put(`${BASE_URL}/faculties`, form);
+        await updateFaculty(form);
+
         toast.current.show({
           severity: "success",
           summary: "Berhasil",
           detail: "Fakultas diperbarui",
         });
       } else {
-        await axios.post(`${BASE_URL}/faculties`, form);
+        await createFaculty(form);
+
         toast.current.show({
           severity: "success",
           summary: "Berhasil",
@@ -134,7 +101,7 @@ const Faculty = () => {
 
   const confirmDelete = (faculty) => {
     confirmDialog({
-      message: `Hapus fakultas ${faculty.name}?`,
+      message: `Hapus fakultas ${faculty.facultyName}?`,
       header: "Konfirmasi",
       icon: "pi pi-exclamation-triangle",
       accept: () => deleteFaculty(faculty.id),
@@ -142,41 +109,23 @@ const Faculty = () => {
   };
 
   const deleteFaculty = async (id) => {
-    await axios.delete(`${BASE_URL}/faculties/${id}`);
+    await deleteFacultyApi(id);
+
     toast.current.show({
       severity: "success",
       summary: "Berhasil",
       detail: "Fakultas dihapus",
     });
     fetchFaculties();
-  };
-
-  const confirmBulkDelete = () => {
-    confirmDialog({
-      message: "Hapus fakultas yang dipilih?",
-      header: "Konfirmasi",
-      icon: "pi pi-exclamation-triangle",
-      accept: bulkDeleteFaculties,
-    });
-  };
-
-  const bulkDeleteFaculties = async () => {
-    const ids = selectedFaculties.map((u) => u.id);
-    await axios.post(`${BASE_URL}/faculties/bulk-delete`, { ids });
-    toast.current.show({
-      severity: "success",
-      summary: "Berhasil",
-      detail: "Fakultas dihapus",
-    });
-    fetchFaculties();
-    setSelectedFaculties([]);
   };
 
   return (
     <div className="p-4">
-      <div className="text-xl font-semibold mb-5">Kelola Fakultas</div>
+      <div className="text-2xl font-semibold mb-5">Kelola Fakultas</div>
+
       <Toast ref={toast} />
       <ConfirmDialog />
+
       <div className="card">
         <Toolbar
           className="mb-4"
@@ -189,14 +138,6 @@ const Faculty = () => {
                 className="mr-4"
                 size="small"
               />
-              {/* <Button
-                label="Hapus"
-                icon="pi pi-trash"
-                severity="danger"
-                disabled={!selectedFaculties.length}
-                onClick={confirmBulkDelete}
-                size="small"
-              /> */}
             </div>
           }
           end={
@@ -216,15 +157,11 @@ const Faculty = () => {
           value={filteredFaculties}
           paginator
           rows={10}
-          selection={selectedFaculties}
-          // onSelectionChange={(e: DataTableSelectionChangeParams) =>
-          //   setSelectedFaculties(e.value)
-          // }
           scrollable
           scrollHeight="460px"
           dataKey={faculties.id}
         >
-          <Column field="nama" header="Nama" sortable />
+          <Column field="facultyName" header="Nama" sortable />
           <Column
             headerStyle={{ textAlign: "right" }}
             body={(row) => (
@@ -264,12 +201,14 @@ const Faculty = () => {
           <label htmlFor="name">Nama</label>
           <InputText
             id="name"
-            value={form.name}
-            onChange={(e) => setForm({ ...form, name: e.target.value })}
-            className={`w-full ${errors.name ? "p-invalid" : ""}`}
+            value={form.facultyName}
+            onChange={(e) => setForm({ ...form, facultyName: e.target.value })}
+            className={`w-full ${errors.facultyName ? "p-invalid" : ""}`}
             size="small"
           />
-          {errors.name && <small className="p-error">{errors.name}</small>}
+          {errors.facultyName && (
+            <small className="p-error">{errors.facultyName}</small>
+          )}
         </div>
       </Dialog>
     </div>
--- src/pages/User.jsx
+++ src/pages/User.jsx
@@ -1,5 +1,4 @@
 import { React, useEffect, useState, useMemo, useRef } from "react";
-import axios from "axios";
 import { DataTable } from "primereact/datatable";
 import { Column } from "primereact/column";
 import { MultiSelect } from "primereact/multiselect";
@@ -12,18 +11,23 @@ import { ConfirmDialog, confirmDialog } from "primereact/confirmdialog";
 import { Dropdown } from "primereact/dropdown";
 import { IconField } from "primereact/iconfield";
 import { InputIcon } from "primereact/inputicon";
+import {
+  createUser,
+  getRoles,
+  getUsers,
+  updateUser,
+  deleteUser as deleteUserApi,
+} from "../api/user";
+import { getFaculties } from "../api/faculty";
 
 const User = () => {
-  const BASE_URL =
-    "https://7fa6-2001-448a-7061-1443-6027-33b2-5c22-a278.ngrok-free.app/api";
-
   const toast = useRef(null);
 
   const [users, setUsers] = useState([]);
   const [roleOptions, setRoleOptions] = useState([]);
   const [facultyOptions, setFacultyOptions] = useState([]);
   const [globalFilter, setGlobalFilter] = useState("");
-  const [selectedUsers, setSelectedUsers] = useState([]);
+  // const [selectedUsers, setSelectedUsers] = useState([]);
   const [dialogVisible, setDialogVisible] = useState(false);
   const [isEditMode, setIsEditMode] = useState(false);
   const [form, setForm] = useState({
@@ -31,19 +35,13 @@ const User = () => {
     userType: null,
     username: "",
     email: "",
+    name: "",
+    nidn: "",
+    nik: "",
+    functionalPosition: "",
+    nim: "",
+    facultyId: null,
     roles: [],
-    dosen: {
-      name: "",
-      nik: "",
-      nidn: "",
-      functionalPosition: "",
-      facultyId: null,
-    },
-    student: {
-      name: "",
-      nim: "",
-      facultyId: null,
-    },
   });
   const [errors, setErrors] = useState({});
 
@@ -53,12 +51,7 @@ const User = () => {
   ];
 
   const fetchUsers = async () => {
-    const res = await axios.get(`${BASE_URL}/users`, {
-      headers: {
-        "ngrok-skip-browser-warning": "any-value",
-      },
-    });
-
+    const res = await getUsers();
     console.log(res.data);
     setUsers(res.data);
   };
@@ -74,11 +67,7 @@ const User = () => {
   }, [users, globalFilter]);
 
   const fetchRoles = async () => {
-    const res = await axios.get(`${BASE_URL}/roles`, {
-      headers: {
-        "ngrok-skip-browser-warning": "any-value",
-      },
-    });
+    const res = await getRoles();
 
     const transformedRoleOptions = res.data.map((role) => ({
       label: role.name.replaceAll("_", " ").toUpperCase(),
@@ -94,14 +83,10 @@ const User = () => {
   }, []);
 
   const fetchFaculties = async () => {
-    const res = await axios.get(`${BASE_URL}/roles`, {
-      headers: {
-        "ngrok-skip-browser-warning": "any-value",
-      },
-    });
+    const res = await getFaculties();
 
     const transformedFacultyOptions = res.data.map((faculty) => ({
-      label: faculty.name,
+      label: faculty.facultyName,
       value: faculty.id,
     }));
 
@@ -117,21 +102,16 @@ const User = () => {
     setIsEditMode(false);
     setForm({
       id: null,
+      userType: null,
       username: "",
       email: "",
+      name: "",
+      nidn: "",
+      nik: "",
+      functionalPosition: "",
+      nim: "",
+      facultyId: null,
       roles: [],
-      dosen: {
-        name: "",
-        nik: "",
-        nidn: "",
-        functionalPosition: "",
-        facultyId: null,
-      },
-      student: {
-        name: "",
-        nim: "",
-        facultyId: null,
-      },
     });
     setErrors({});
     setDialogVisible(true);
@@ -146,6 +126,12 @@ const User = () => {
       userType: user.userType,
       username: user.username,
       email: user.email,
+      name: user.dosen?.name,
+      nidn: user.dosen?.nidn,
+      nik: user.dosen?.nik,
+      functionalPosition: user.dosen?.functionalPosition,
+      nim: user.student?.nim,
+      facultyId: user.faculty.id,
       roles: selectedRoles,
     });
     setErrors({});
@@ -159,17 +145,48 @@ const User = () => {
 
   const saveUser = async () => {
     console.log(form);
+    const payload = {
+      userType: form.userType,
+      username: form.username,
+      email: form.email,
+      roles: form.roles,
+      dosen: null,
+      student: null,
+    };
+
+    if (form.userType === "DOSEN_STAFF") {
+      payload.dosen = {
+        name: form.name,
+        nik: form.nik,
+        nidn: form.nidn,
+        functionalPosition: form.functionalPosition,
+        facultyId: form.facultyId,
+      };
+    }
+
+    if (form.userType === "STUDENT") {
+      payload.student = {
+        name: form.name,
+        nim: form.nim,
+        facultyId: form.facultyId,
+        // programStudyId: form.programStudyId,
+      };
+    }
+
+    console.log("Payload yang dikirim:", payload);
+    // return;
 
     try {
       if (isEditMode) {
-        await axios.put(`${BASE_URL}/users`, form);
+        payload.id = form.id;
+        await updateUser(payload);
         toast.current.show({
           severity: "success",
           summary: "Berhasil",
           detail: "Pengguna diperbarui",
         });
       } else {
-        await axios.post(`${BASE_URL}/users`, form);
+        await createUser(payload);
         toast.current.show({
           severity: "success",
           summary: "Berhasil",
@@ -185,7 +202,7 @@ const User = () => {
 
   const confirmDelete = (user) => {
     confirmDialog({
-      message: `Hapus pengguna ${user.name}?`,
+      message: `Hapus pengguna ${user.username}?`,
       header: "Konfirmasi",
       icon: "pi pi-exclamation-triangle",
       accept: () => deleteUser(user.id),
@@ -193,7 +210,7 @@ const User = () => {
   };
 
   const deleteUser = async (id) => {
-    await axios.delete(`${BASE_URL}/users/${id}`);
+    await deleteUserApi(id);
     toast.current.show({
       severity: "success",
       summary: "Berhasil",
@@ -202,30 +219,30 @@ const User = () => {
     fetchUsers();
   };
 
-  const confirmBulkDelete = () => {
-    confirmDialog({
-      message: "Hapus pengguna yang dipilih?",
-      header: "Konfirmasi",
-      icon: "pi pi-exclamation-triangle",
-      accept: bulkDeleteUsers,
-    });
-  };
+  // const confirmBulkDelete = () => {
+  //   confirmDialog({
+  //     message: "Hapus pengguna yang dipilih?",
+  //     header: "Konfirmasi",
+  //     icon: "pi pi-exclamation-triangle",
+  //     accept: bulkDeleteUsers,
+  //   });
+  // };
 
-  const bulkDeleteUsers = async () => {
-    const ids = selectedUsers.map((u) => u.id);
-    await axios.post(`${BASE_URL}/users/bulk-delete`, { ids });
-    toast.current.show({
-      severity: "success",
-      summary: "Berhasil",
-      detail: "Pengguna dihapus",
-    });
-    fetchUsers();
-    setSelectedUsers([]);
-  };
+  // const bulkDeleteUsers = async () => {
+  //   const ids = selectedUsers.map((u) => u.id);
+  //   await axios.post(`${BASE_URL}/users/bulk-delete`, { ids });
+  //   toast.current.show({
+  //     severity: "success",
+  //     summary: "Berhasil",
+  //     detail: "Pengguna dihapus",
+  //   });
+  //   fetchUsers();
+  //   setSelectedUsers([]);
+  // };
 
   return (
     <div className="p-4">
-      <div className="text-xl font-semibold mb-5">Kelola Pengguna</div>
+      <div className="text-2xl font-semibold mb-5">Kelola Pengguna</div>
       <Toast ref={toast} />
       <ConfirmDialog />
       <div className="card">
@@ -267,13 +284,13 @@ const User = () => {
           value={filteredUsers}
           paginator
           rows={10}
-          selection={selectedUsers}
+          // selection={selectedUsers}
           // onSelectionChange={(e: DataTableSelectionChangeParams) =>
           //   setSelectedUsers(e.value)
           // }
           scrollable
           scrollHeight="460px"
-          dataKey={users.id}
+          dataKey="id"
         >
           {/* <Column
             selectionMode="multiple"
@@ -353,7 +370,9 @@ const User = () => {
             className={`w-full ${errors.name ? "p-invalid" : ""}`}
             size="small"
           />
-          {errors.name && <small className="p-error">{errors.name}</small>}
+          {errors.username && (
+            <small className="p-error">{errors.username}</small>
+          )}
         </div>
         <div className="field mb-3">
           <label htmlFor="email">Email</label>
@@ -366,21 +385,73 @@ const User = () => {
           />
           {errors.email && <small className="p-error">{errors.email}</small>}
         </div>
+        <div className="field mb-3">
+          <label htmlFor="name">Nama</label>
+          <InputText
+            id="name"
+            value={form.name}
+            onChange={(e) => setForm({ ...form, name: e.target.value })}
+            className={`w-full ${errors.name ? "p-invalid" : ""}`}
+            size="small"
+          />
+          {errors.name && <small className="p-error">{errors.name}</small>}
+        </div>
         {form.userType === "DOSEN_STAFF" && (
-          <div className="field mb-3">
-            <label htmlFor="role">Peran</label>
-            <MultiSelect
-              value={form.roles}
-              onChange={(e) => setForm({ ...form, roles: e.value })}
-              options={roleOptions}
-              optionLabel="label"
-              placeholder="Pilih Peran"
-              // maxSelectedLabels={3}
-              className="w-full md:w-20rem"
-              size="small"
-            />
-            {errors.role && <small className="p-error">{errors.role}</small>}
-          </div>
+          <>
+            <div className="field mb-3">
+              <label htmlFor="nidn">NIDN</label>
+              <InputText
+                id="nidn"
+                value={form.nidn}
+                onChange={(e) => setForm({ ...form, nidn: e.target.value })}
+                className={`w-full ${errors.nidn ? "p-invalid" : ""}`}
+                size="small"
+              />
+              {errors.nidn && <small className="p-error">{errors.nidn}</small>}
+            </div>
+            <div className="field mb-3">
+              <label htmlFor="nik">NIK</label>
+              <InputText
+                id="nik"
+                value={form.nik}
+                onChange={(e) => setForm({ ...form, nik: e.target.value })}
+                className={`w-full ${errors.nik ? "p-invalid" : ""}`}
+                size="small"
+              />
+              {errors.nik && <small className="p-error">{errors.nik}</small>}
+            </div>
+            <div className="field mb-3">
+              <label htmlFor="functionalPosition">Jabatan</label>
+              <InputText
+                id="functionalPosition"
+                value={form.functionalPosition}
+                onChange={(e) =>
+                  setForm({ ...form, functionalPosition: e.target.value })
+                }
+                className={`w-full ${
+                  errors.functionalPosition ? "p-invalid" : ""
+                }`}
+                size="small"
+              />
+              {errors.functionalPosition && (
+                <small className="p-error">{errors.functionalPosition}</small>
+              )}
+            </div>
+            <div className="field mb-3">
+              <label htmlFor="role">Peran</label>
+              <MultiSelect
+                value={form.roles}
+                onChange={(e) => setForm({ ...form, roles: e.value })}
+                options={roleOptions}
+                optionLabel="label"
+                placeholder="Pilih Peran"
+                // maxSelectedLabels={3}
+                className="w-full md:w-20rem"
+                size="small"
+              />
+              {errors.role && <small className="p-error">{errors.role}</small>}
+            </div>
+          </>
         )}
 
         {form.userType === "STUDENT" && (
@@ -396,33 +467,23 @@ const User = () => {
               />
               {errors.nim && <small className="p-error">{errors.nim}</small>}
             </div>
-            <div className="field mb-3">
-              <label htmlFor="name">Nama</label>
-              <InputText
-                id="name"
-                value={form.name}
-                onChange={(e) => setForm({ ...form, name: e.target.value })}
-                className={`w-full ${errors.name ? "p-invalid" : ""}`}
-                size="small"
-              />
-              {errors.name && <small className="p-error">{errors.name}</small>}
-            </div>
-            <div className="field mb-3">
-              <label htmlFor="faculty">Fakultas</label>
-              <Dropdown
-                id="faculty"
-                options={facultyOptions}
-                onChange={(e) => setForm({ ...form, faculty: e.value })}
-                placeholder="Pilih Peran"
-                className={`w-full ${errors.faculty ? "p-invalid" : ""}`}
-                size="small"
-              />
-              {errors.faculty && (
-                <small className="p-error">{errors.faculty}</small>
-              )}
-            </div>
           </>
         )}
+        <div className="field mb-3">
+          <label htmlFor="faculty">Fakultas</label>
+          <Dropdown
+            id="faculty"
+            options={facultyOptions}
+            value={form.facultyId}
+            onChange={(e) => setForm({ ...form, facultyId: e.value })}
+            placeholder="Pilih Fakultas"
+            className={`w-full ${errors.faculty ? "p-invalid" : ""}`}
+            size="small"
+          />
+          {errors.faculty && (
+            <small className="p-error">{errors.faculty}</small>
+          )}
+        </div>
       </Dialog>
     </div>
   );
