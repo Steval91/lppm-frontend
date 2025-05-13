8c27c26d3fd64819c29a636f97f566557f59be48
Steval91 <167100080+Steval91@users.noreply.github.com>
2025-05-13 22:41:00 +0700

    Prevent admin role from accessing non admin route.

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,18 @@ import React from "react";
 import { Routes, Route, Navigate } from "react-router-dom";
 import ProtectedRoute from "./routes/ProtectedRoute";
 import AdminRoute from "./routes/AdminRoute";
+import NonAdminRoute from "./routes/NonAdminRoute";
 import MainLayout from "./layout/MainLayout";
 import Login from "./pages/Login";
 import Dashboard from "./pages/Dashboard";
 import Proposal from "./pages/Proposal";
+import Review from "./pages/Review";
+import Approval from "./pages/Approval";
 import FacultyHead from "./pages/FacultyHeadDashboard";
 import User from "./pages/User";
 import Faculty from "./pages/Faculty";
 import FacultyHeadDashboard from "./pages/FacultyHeadDashboard";
 import ReviewerDashboard from "./pages/ReviewerDashboard";
-import Review from "./pages/Review";
 
 const App = () => {
   return (
@@ -26,9 +28,38 @@ const App = () => {
         }
       >
         <Route index element={<Dashboard />} />
-        <Route path="my-proposals" element={<Dashboard />} />
-        <Route path="proposals" element={<Proposal />} />
-        <Route path="reviews" element={<Review />} />
+        <Route
+          path="my-proposals"
+          element={
+            <NonAdminRoute>
+              <Dashboard />
+            </NonAdminRoute>
+          }
+        />
+        <Route
+          path="proposals"
+          element={
+            <NonAdminRoute>
+              <Proposal />
+            </NonAdminRoute>
+          }
+        />
+        <Route
+          path="reviews"
+          element={
+            <NonAdminRoute>
+              <Review />
+            </NonAdminRoute>
+          }
+        />
+        <Route
+          path="approvals"
+          element={
+            <NonAdminRoute>
+              <Approval />
+            </NonAdminRoute>
+          }
+        />
         <Route
           path="users"
           element={
--- src/components/SidebarMenu.jsx
+++ src/components/SidebarMenu.jsx
@@ -5,11 +5,12 @@ import ProfileDropdown2 from "./ProfileDropdown2";
 import { useAuth } from "../contexts/AuthContext";
 
 const menuItems = [
-  { label: "Dashboard", path: "/", icon: "pi pi-th-large" },
+  // { label: "Dashboard", path: "/", icon: "pi pi-th-large" },
   // { label: "Proposal Saya", path: "/my-proposals", icon: "pi pi-book" },
   // { label: "Pengajuan", path: "/proposals", icon: "pi pi-user-edit" },
-  { label: "Proposal", path: "/proposals", icon: "pi pi-book" },
-  { label: "Review", path: "/reviews", icon: "pi pi-check-square" },
+  { label: "Pengajuan", path: "/proposals", icon: "pi pi-book" },
+  { label: "Penilaian", path: "/reviews", icon: "pi pi-user-edit" },
+  // { label: "Persetujuan", path: "/approvals", icon: "pi pi-check-square" },
   // {
   //   label: "Laporan Progres",
   //   path: "/progress-reports",
@@ -30,36 +31,42 @@ const SidebarMenu = () => {
 
   return (
     <div className="w-72 bg-white shadow-sm flex flex-col">
-      <div className="p-4 text-2xl font-bold"></div>
+      <div className="p-4 text-2xl font-bold">LPPM UNKLAB</div>
 
       <div className="flex-1 overflow-y-auto">
-        <div className="px-4 py-2 text-md font-semibold text-gray-600 uppercase">
-          Penelitian
-        </div>
-        <nav className="flex flex-col gap-1 px-2">
-          {menuItems.map((item) => (
-            <NavLink
-              key={item.path}
-              to={item.path}
-              end
-              className={({ isActive }) =>
-                classNames(
-                  "flex items-center gap-3 px-3 py-2 rounded-lg text-md transition-colors",
-                  isActive
-                    ? "bg-blue-100 text-blue-600 font-medium"
-                    : "text-gray-700 hover:bg-gray-100"
-                )
-              }
-            >
-              <i className={item.icon}></i>
-              {item.label}
-            </NavLink>
-          ))}
-        </nav>
+        {!isAdmin && (
+          <>
+            <div className="mb-6">
+              <div className="px-4 py-2 text-md font-semibold text-gray-600 uppercase">
+                Penelitian
+              </div>
+              <nav className="flex flex-col gap-1 px-2">
+                {menuItems.map((item) => (
+                  <NavLink
+                    key={item.path}
+                    to={item.path}
+                    end
+                    className={({ isActive }) =>
+                      classNames(
+                        "flex items-center gap-3 px-3 py-2 rounded-lg text-md transition-colors",
+                        isActive
+                          ? "bg-blue-100 text-blue-600 font-medium"
+                          : "text-gray-700 hover:bg-gray-100"
+                      )
+                    }
+                  >
+                    <i className={item.icon}></i>
+                    {item.label}
+                  </NavLink>
+                ))}
+              </nav>
+            </div>
+          </>
+        )}
 
         {isAdmin && (
           <>
-            <div className="px-4 mt-6 py-2 text-md font-semibold text-gray-600 uppercase">
+            <div className="px-4 py-2 text-md font-semibold text-gray-600 uppercase">
               Pengaturan
             </div>
             <nav className="flex flex-col gap-1 px-2">
