2f48b1d26658eb6783c3f5df3f6523b4384e53c2
Steval91 <167100080+Steval91@users.noreply.github.com>
2025-05-13 13:04:11 +0700

    Update proposal schema validation.

--- src/validationSchemas/proposalSchema.js
+++ src/validationSchemas/proposalSchema.js
@@ -1,6 +1,6 @@
 import { z } from "zod";
 
-const fileSizeLimit = 5 * 1024 * 1024; // 5MB
+// const fileSizeLimit = 5 * 1024 * 1024; // 5MB
 
 // Definisikan schema validasi untuk proposal
 const proposalSchema = z.object({
@@ -8,18 +8,32 @@ const proposalSchema = z.object({
   ketuaPeneliti: z.number().min(1, "Ketua Peneliti wajib dipilih"),
   anggotaDosen: z.array(z.number()).min(1, "Minimal 1 Anggota Dosen"),
   anggotaMahasiswa: z.array(z.number()).optional(),
-  waktuPelaksanaan: z.string().min(1, "Waktu Pelaksanaan wajib dipilih"),
+  waktuPelaksanaan: z.preprocess(
+    (val) => {
+      if (val === "" || val == null) return undefined;
+      return new Date(val);
+    },
+    z.date({
+      required_error: "Waktu Pelaksanaan wajib dipilih",
+      invalid_type_error: "Format Waktu Pelaksanaan tidak valid",
+    })
+  ),
+
   sumberDana: z.string().min(1, "Sumber Dana wajib diisi"),
-  danaYangDiUsulkan: z.number().nonnegative().min(1, "Jumlah Dana wajib diisi"),
-  fileUrl: z
-    .any()
-    // .instanceof(File)
-    // .refine((file) => ["application/pdf"].includes(file.type), {
-    //   message: "Tipe file harus PDF",
-    // })
-    .refine((file) => file.size <= fileSizeLimit, {
-      message: "Ukuran file tidak boleh lebih 5MB",
-    }),
+  danaYangDiUsulkan: z
+    .number({
+      required_error: "Jumlah Dana wajib dipilih",
+      invalid_type_error: "Format Jumlah Dana tidak valid",
+    })
+    .nonnegative(),
+  fileUrl: z.any(),
+  // .instanceof(File)
+  // .refine((file) => ["application/pdf"].includes(file.type), {
+  //   message: "Tipe file harus PDF",
+  // })
+  // .refine((file) => file.size <= fileSizeLimit, {
+  //   message: "Ukuran file tidak boleh lebih 5MB",
+  // }),
   luaranPenelitian: z.string().min(1, "Luaran Penelitian wajib diisi"),
   namaMitra: z.string().optional(),
   alamatMitra: z.string().optional(),
